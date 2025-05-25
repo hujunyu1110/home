335401c9a06229833044ff762c5a04394eef9a56
louny <hujunyu1110@foxmail.com>
2025-05-25 09:25:43 +0800

    change phone

--- script/utils/common.js
+++ script/utils/common.js
@@ -6,18 +6,11 @@ function openPage(url) {
 function judgeScreen() {
     var he = document.body.clientHeight;
     var wi = document.body.clientWidth;
-    //console.log(1, he, wi);
     if (he < wi) { }
     else {
-
-        $(".all_link").attr("style", "transform: scale(1.3); margin-left:10%; margin-top:10%;");
-
-        $("p").attr("style", "font-size:23px;");
-
         $(".searchImgEx4").removeClass("col-sm-4");
         $(".searchImgEx5").removeClass("col-sm-5");
         $(".searchImgEx4").addClass("col-sm-3");
-        //$(".searchImgEx5").addClass("col-sm-1");
         $(".searchImg").removeClass("col-sm-1");
         $(".searchImg").addClass("col-sm-3");
 
@@ -26,10 +19,6 @@ function judgeScreen() {
         $(".searchCh").removeClass("col-xs-6 col-sm-5");
         $(".searchCh").addClass("col-xs-6 col-sm-9");
 
-        $("#search").text("搜索");
-        $("#Oinput").attr("style", "height: 80px; font-size: 27px; margin-bottom: 80px;");
-        $("#search").attr("style", "height: 80px; font-size: 27px; margin-bottom: 80px;");
-        //$(".searImg").attr("style", "height: 30px;");
         $(".firstFlowcc").attr("style", "width: 15%;")
         $(".baiduclass").attr("style", "height: 30px;margin-left: 8px");
         $(".bingclass").attr("style", " height: 30px; margin-top:2px; margin-left: 14%;");
